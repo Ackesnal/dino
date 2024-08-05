f65c62e3a654eb61a4792b5223444fa679deb12b
uqxxu16 <uqxxu16@wiener.hpc.dc.uq.edu.au>
2024-08-05 21:42:58 +1000

    updated

--- vision_transformer.py
+++ vision_transformer.py
@@ -204,7 +204,7 @@ class Attention(nn.Module):
         self.scale = qk_scale or head_dim ** -0.5
 
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop)
+        self.attn_drop = attn_drop # nn.Dropout(attn_drop)
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
 
@@ -212,12 +212,16 @@ class Attention(nn.Module):
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         q, k, v = qkv[0], qkv[1], qkv[2]
-
+        
+        x = nn.functional.scaled_dot_product_attention(q, k, v, dropout_p=self.attn_drop)
+        x = x.transpose(1, 2).reshape(B, N, C)
+        """
         attn = (q @ k.transpose(-2, -1)) * self.scale
         attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
-
         x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        """
+        
         x = self.proj(x)
         x = self.proj_drop(x)
         return x, attn
